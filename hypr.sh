2ef9c9a53e8222bb09cac5d19893c45c3ac6c429
gnogeek <77178414+gnogeek@users.noreply.github.com>
2023-05-03 14:17:19 -0400

    Create hypr.sh

---
+++ hypr.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+yay -S --noconfirm --needed hyprland kitty jq mako waybar-hyprland swww swaylock-effects wofi wlogout xdg-desktop-portal-hyprland swappy grim slurp thunar polkit-gnome python-requests pamixer pavucontrol brightnessctl bluez bluez-utils blueman gvfs thunar-archive-plugin file-roller btop pacman-contrib ttf-jetbrains-mono-nerd noto-fonts-emoji lxappearance xfce4-settings sddm-git sddm-sugar-candy-git 
