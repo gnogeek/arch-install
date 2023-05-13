45f656a6167b67e72990d7c3afcb36d398e57bea
gnogeek <77178414+gnogeek@users.noreply.github.com>
2023-05-13 18:36:36 -0400

    Creating packages list

---
+++ packages.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#Install Fonts
+
+yay -S adobe-source-code-pro-fonts ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common
+
+#Installaudio components 
+yay -S pipewire wireplumber pavucontrol pipewire-audio pipewire-pulse
